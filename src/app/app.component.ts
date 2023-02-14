8314c42f26913b2039b49c779905618a9b9106ef
rohit2710-on <rohitc@rknec.edu>
2023-02-14 16:47:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { DateServiceService } from './date-service.service';
+import { BookServiceService } from './book-service.service';
+import { book } from './book';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'day5Services';
-}
+  today:any;
+  
+  myInputMessage:string="From parent"
+  name:string="";
+  books:book[]=[];
+  message:string="";
+  courseReg(courseName:string){
+    this.message=`Your registration for ${courseName} is successfull`;
+  }
+  // GetOutput(data:any){
+    // console.log(data);
+
+  // }
+
+  // constructor( private dateservice:DateServiceService, private bookservice: BookServiceService){
+    
+   
+  }
+
+  
+  // ngOnInit(){
+  //   this.today=this.dateservice.getTodayDate()
+  //   this.books=this.bookservice.getAllBooks()
+
+  // }
+
